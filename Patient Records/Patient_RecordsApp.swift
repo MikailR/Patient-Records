052062b5f426b96365631e49a28836f388cc34bc
Mikail <mikailrahman1@hotmail.com>
2023-06-11 18:37:26 -0400

    Initial Commit

---
+++ Patient Records/Patient_RecordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Patient_RecordsApp.swift
+//  Patient Records
+//
+//  Created by Mikail Rahman on 2023-06-11.
+//
+
+import SwiftUI
+
+@main
+struct Patient_RecordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
